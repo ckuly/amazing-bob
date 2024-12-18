5c7051913ea6884142c5ae2c344222dc3c8d5f22
ckul <88058944+ckuly@users.noreply.github.com>
2024-12-18 16:37:16 +0200

    Some great changes made

--- phandling/urls.py
+++ phandling/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('releases/', views.releases, name='releases'),
     path('user_projects/', views.user_projects, name='user_projects'),
     path('projects/', views.project_dashboard, name='project_dashboard'),
+    path('register/', views.register, name='register'),
 ]
--- phandling/views.py
+++ phandling/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Project
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.contrib.auth.forms import User
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
 
 def home(request):
     return render(request, 'base.html')
@@ -22,3 +26,32 @@ def project_dashboard(request):
 def register(request):
     form = UserCreationForm()
     return render(request, 'registration/login.html', {"form": form})
+
+@csrf_protect
+def register(request):
+    if request.method == "POST":
+        # pasiimame reikšmes iš registracijos formos
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        # tikriname, ar sutampa slaptažodžiai
+        if password == password2:
+            # tikriname, ar neužimtas username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, f'Vartotojo vardas {username} užimtas!')
+                return redirect('register')
+            else:
+                # tikriname, ar nėra tokio pat email
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, f'Vartotojas su el. paštu {email} jau užregistruotas!')
+                    return redirect('register')
+                else:
+                    # jeigu viskas tvarkoje, sukuriame naują vartotoją
+                    User.objects.create_user(username=username, email=email, password=password)
+                    messages.info(request, f'Vartotojas {username} užregistruotas!')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Slaptažodžiai nesutampa!')
+            return redirect('register')
+    return render(request, 'register.html')
